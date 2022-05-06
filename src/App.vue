0c11853c10d55ecab30b19209f8b1c2895209e04
Mazen <mazenadel19@gmail.com>
2022-05-06 11:58:48 +0200

    add bootstrap & fontawesome + banner & cards

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,85 @@
-<script setup></script>
+<script setup>
+import products from "./assets/products.json";
+</script>
 
 <template>
-  <router-link to="/">Welcome</router-link>|
-  <router-link to="/lec1">Lecture 1</router-link>|
-  <router-link to="/lec2">Lecture 2</router-link>|
-  <router-link to="/lec3">Lecture 3</router-link>|
-  <router-link to="/lec4">Lecture 4</router-link>
-  <router-view></router-view>
+  <div class="container">
+    <!-- Start Banner -->
+    <div class="row">
+      <div
+        class="col-md-12 bg-dark text-light d-flex justify-content-between align-items-baseline p-3"
+      >
+        <a href="#" class="text-warning">Vue-Store</a>
+        <div class="d-flex align-items-baseline">
+          <p class="lead mb-0">[0] items in your chart with total price [0]$</p>
+          <button class="btn btn-primary ms-3">show chart</button>
+        </div>
+      </div>
+    </div>
+    <!-- End Banner -->
+
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4 mt-1">
+      <div class="col" v-for="product in products" :key="product.id">
+        <div class="card">
+          <img :src="product.image" class="card-img-top" :alt="product.name" />
+          <div class="card-body text-center">
+            <h5 class="card-title">{{ shortifyText(product.name, 3) }}</h5>
+            <p class="card-text">
+              {{ shortifyText(product.description, 15) }}
+            </p>
+          </div>
+          <div class="card-footer d-flex flex-column">
+            <div class="row">
+              <p class="badge text-primary center col">
+                InStock: <span>{{ product.instock }}</span>
+              </p>
+              <p class="col center">Price: {{ product.price }}</p>
+            </div>
+            <div class="row">
+              <p class="col center">
+                Rating: <span>{{ product.rate }}</span>
+              </p>
+              <button class="col btn btn-success btn-sm" :disabled="product.instock===0">Add To Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {},
+  methods: {
+    shortifyText: (text, num) => {
+      const tArray = text.split(" ");
+      if (tArray.length > num) {
+        let newText = "";
+        for (let index = 0; index < num; index++) {
+          const word = tArray[index];
+          newText += word + " ";
+        }
+        return newText + "...";
+      } else {
+        return text;
+      }
+
+      // return text.slice(200)+"..."
+    },
+  },
+};
+</script>
 
 <style>
 @import "@/assets/base.css";
+img {
+  object-fit: contain;
+  height: 250px;
+}
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+
 import { createApp } from "vue";
+
 import App from "./App.vue";
 import router from "./router";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Scripts
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 
 const app = createApp(App);
 
