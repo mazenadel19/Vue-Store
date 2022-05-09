f10ab21e689093dcef359e0e30d64d0dd5f95b06
Mazen <mazenadel19@gmail.com>
2022-05-09 02:28:00 +0200

    working on cart, add to cart

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import products from "./assets/products.json";
+import allproducts from "./assets/products.json";
 </script>
 
 <template>
@@ -9,19 +9,32 @@ import products from "./assets/products.json";
       <div
         class="col-md-12 bg-dark text-light d-flex justify-content-between align-items-baseline p-3"
       >
-        <a href="#" class="text-warning text-decoration-none" @click.prevent="hideCart">Vue-Store</a>
+        <a
+          href="#"
+          class="text-warning text-decoration-none"
+          @click.prevent="hideCart"
+          >Vue-Store</a
+        >
         <div class="d-flex align-items-baseline">
-          <p class="lead mb-0">[0] items in your chart with total price [0]$</p>
-          <button class="btn btn-primary ms-3"
-          @click="showCart"
-          >show chart</button>
+          <p class="lead mb-0">
+            {{ cart.items.length }} item<template v-if="cart.items.length !== 1"
+              >s</template
+            >
+            in your chart with total price {{ currency(cart.cartTotal) }}
+          </p>
+          <button class="btn btn-primary ms-3" @click="showCart">
+            show chart
+          </button>
         </div>
       </div>
     </div>
     <!-- End Banner -->
 
     <!-- Start Products -->
-    <div v-show="!isShowCart" class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4 mt-1">
+    <div
+      v-show="!isShowCart"
+      class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4 mt-1"
+    >
       <div class="col" v-for="product in products" :key="product.id">
         <div class="card">
           <img :src="product.image" class="card-img-top" :alt="product.name" />
@@ -59,11 +72,12 @@ import products from "./assets/products.json";
             </div>
             <div class="row">
               <p class="col center">
-                Rating: <span>{{ product.rate }}</span>
+                Rating: <span class="font_20">{{ product.rate }}</span>
               </p>
               <button
                 class="col btn btn-success btn-sm"
                 :disabled="product.instock === 0"
+                @click="addToCart(product)"
               >
                 Add To Cart
               </button>
@@ -73,11 +87,50 @@ import products from "./assets/products.json";
       </div>
     </div>
     <!-- End Products -->
+
     <!-- Start Cart -->
     <div class="row" v-show="isShowCart">
+      <!-- Start Header -->
       <div class="col-12">
-        <h1>You cart</h1>
+        <div class="p-5 border mb-4 bg-light rounded-3">
+          <div class="container-fluid py-5">
+            <h1 v-if="cart.items.length" class="display-5 fw-bold text-center">
+              Your Cart <i class="fa-brands fa-opencart"></i>
+            </h1>
+            <h1 v-else class="display-5 fw-bold text-center">
+              Your Cart is Empty
+            </h1>
+          </div>
+        </div>
       </div>
+      <!-- End Header -->
+
+      <!-- Start Cart Products -->
+      <div class="container" v-show="cart.items.length">
+        <div class="row">
+          <div class="col-12">
+            <table class="table table-striped text-center">
+              <thead>
+                <tr>
+                  <th scope="col">Product Name</th>
+                  <th scope="col">Product Quantity</th>
+                  <th scope="col">Product Price</th>
+                  <th scope="col">Product Total Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in cart.items" :key="item.id">
+                  <td>{{ item.name }}</td>
+                  <td>{{ item.quantity }}</td>
+                  <td>{{ currency(item.price) }}</td>
+                  <th>{{ currency(item.totalItemPrice) }}</th>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <!-- End Cart Products-->
     </div>
     <!-- End Cart -->
   </div>
@@ -87,9 +140,11 @@ import products from "./assets/products.json";
 export default {
   data() {
     return {
+      products: allproducts,
       isShowCart: false,
       cart: {
         items: [],
+        cartTotal: 0,
       },
     };
   },
@@ -115,12 +170,33 @@ export default {
       });
       return formatter.format(value);
     },
-    showCart:function(){
-      this.isShowCart=true
+    addToCart: function (product) {
+      const { items } = this.cart;
+      const existingItem = items.find((item) => item.id === product.id);
+      // cart logic
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.totalItemPrice += existingItem.price;
+      } else {
+        items.push({
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          quantity: 1,
+          totalItemPrice: product.price,
+        });
+      }
+      this.cart.cartTotal = this.cart.cartTotal + product.price;
+      // product ui logic
+      const existingProduct = this.products.find((p) => p.id === product.id);
+      existingProduct.instock--;
     },
-    hideCart:function(){
-      this.isShowCart=false
-    }
+    showCart: function () {
+      this.isShowCart = true;
+    },
+    hideCart: function () {
+      this.isShowCart = false;
+    },
   },
 };
 </script>
@@ -139,6 +215,7 @@ img {
 
 .font_20 {
   font-size: 20px;
+  margin-left: 5px;
 }
 
 .instock_more {
@@ -168,5 +245,19 @@ img {
   );
   box-shadow: gray -5px -5px 10px;
   cursor: pointer;
+}
+
+th {
+  font-weight: bold;
+}
+
+thead {
+  white-space: nowrap;
+}
+
+tr td,
+th {
+  text-overflow: ellipsis;
+  vertical-align: middle;
 }
 </style>
