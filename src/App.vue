a70534b70b47b71d8bf090853be663be8ae2bcd1
Mazen <mazenadel19@gmail.com>
2022-07-27 18:55:32 +0200

    header style

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,42 @@ import allproducts from "./assets/products.json";
 </script>
 
 <template>
-  <div class="container">
-    <!-- Start Banner -->
-    <div class="row">
-      <div
-        class="col-md-12 bg-dark text-light d-flex justify-content-between align-items-baseline p-3"
-      >
-        <a
-          href="#"
-          class="text-warning text-decoration-none"
-          @click.prevent="hideCart"
-          >Vue-Store</a
+  <!-- Start Banner -->
+  <header class="bg-dark">
+    <div class="container">
+      <div class="row">
+        <div
+          class="col-md-12 text-light d-flex justify-content-between align-items-baseline p-3"
         >
-        <div class="d-flex align-items-baseline">
-          <p class="lead mb-0">
-            {{ cart.items.length }} item<template v-if="cart.items.length !== 1"
-              >s</template
+          <a
+            href="#"
+            class="text-warning text-decoration-none"
+            @click.prevent="hideCart"
+            >Vue-Store</a
+          >
+          <div class="d-flex align-items-baseline">
+            <button
+              class="btn btn-primary ms-3"
+              @click="showCart"
+              :title="
+                cart.items.length +
+                ` item${
+                  cart.items.length !== 1 ? 's' : ''
+                } in your chart with total price ` +
+                currency(cart.cartTotal)
+              "
             >
-            in your chart with total price {{ currency(cart.cartTotal) }}
-          </p>
-          <button class="btn btn-primary ms-3" @click="showCart">
-            show chart
-          </button>
+              show chart
+            </button>
+          </div>
         </div>
       </div>
     </div>
-    <!-- End Banner -->
+  </header>
+  <!-- End Banner -->
 
-    <!-- Start Products -->
+  <!-- Start Products -->
+  <div class="container">
     <div
       v-show="!isShowCart"
       class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4 mt-1"
@@ -99,6 +107,11 @@ import allproducts from "./assets/products.json";
             </h1>
             <h1 v-else class="display-5 fw-bold text-center">
               Your Cart is Empty
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                <path
+                  d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z"
+                />
+              </svg>
             </h1>
           </div>
         </div>
